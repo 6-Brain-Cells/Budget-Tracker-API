e434fa1e9f6f3bbdd1ae58f9760534a00f1a4645
YoussefOS <youssefosamame@gmail.com>
2025-12-03 18:42:44 +0200

    try to activate swagger 2

--- Budget Tracker API/Program.cs
+++ Budget Tracker API/Program.cs
@@ -4,34 +4,40 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+// --- FIX 1: Remove conflicting OpenApi lines ---
+// You were mixing the new Microsoft OpenAPI and Swashbuckle. 
+// Stick to Swashbuckle (AddSwaggerGen) for now as it works best with SwaggerUI.
+// builder.Services.AddOpenApi(); <--- REMOVE THIS
+
+builder.Services.AddEndpointsApiExplorer(); // Add this line (required for minimal APIs/Swagger)
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-   app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty;
-    });
-//}
+
+// --- FIX 2: Ensure Swagger runs in Production ---
+// app.MapOpenApi(); <--- REMOVE THIS (Conflict)
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    // --- FIX 3: REMOVE THE LEADING SLASH ---
+    // Change "/swagger/v1/swagger.json" to "swagger/v1/swagger.json"
+    // This fixes the 404 error if your app is hosted in a sub-folder.
+    c.SwaggerEndpoint("swagger/v1/swagger.json", "My API V1");
+
+    // Keep this to make Swagger the homepage
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
